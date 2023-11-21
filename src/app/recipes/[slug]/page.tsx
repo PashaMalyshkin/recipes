01b807511cce83d6f872d62beabc69ba600c35be
Pasha <pashamalyshkin2003@gmail.com>
2023-11-21 11:41:26 +0200

    added main tag with paddings to details page

--- src/app/recipes/[slug]/page.tsx
+++ src/app/recipes/[slug]/page.tsx
@@ -3,8 +3,10 @@ import { Suspense } from "react";
 import { RecipeDetailsCardSkeleton } from "~/app/_components/recipes/recipe-details-card-skeleton";
 export default function DetailsPage() {
   return (
-    <Suspense fallback={<RecipeDetailsCardSkeleton />}>
-      <RecipeDetailsCard />;
-    </Suspense>
+    <main className="py-6">
+      <Suspense fallback={<RecipeDetailsCardSkeleton />}>
+        <RecipeDetailsCard />;
+      </Suspense>
+    </main>
   );
 }
